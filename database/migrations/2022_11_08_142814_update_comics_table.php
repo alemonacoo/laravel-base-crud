949c3a60278479e8b9eb039de0d24a07ed803f8b
alemonacoo <ale.monaco@outlook.com>
2022-11-08 15:41:24 +0100

    update_migration: 'definition' from string to text

---
+++ database/migrations/2022_11_08_142814_update_comics_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Comics', function (Blueprint $table) {
+            //
+            $table->text('description')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Comics', function (Blueprint $table) {
+            //
+            $table->string('description')->change();
+        });
+    }
+}
