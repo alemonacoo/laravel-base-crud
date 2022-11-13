e422e2715e38ee03f561d112fa205b5e0b1821f2
alemonacoo <ale.monaco@outlook.com>
2022-11-13 19:11:58 +0100

    adding basic validations in edit + create pt.2

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -7,6 +7,15 @@
     <title>Create Pasta</title>
 </head>
 <body>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
     <div>
         <form method="POST" action="{{ route('comics.store') }}">
             @csrf
